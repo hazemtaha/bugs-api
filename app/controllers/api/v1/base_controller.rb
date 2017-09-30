7c23188e05c50387af4b1f5acfc9c09ce9cb3344
Hazem Taha <thatskan@gmail.com>
2017-10-01 01:55:37 +0200

    catch ArguemtnError exception and render appropriate json response

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::BaseController < ApplicationController
   rescue_from ActionController::ParameterMissing, with: :render_unprocessable_entity
+  rescue_from ArgumentError, with: :render_unprocessable_entity
 
   def render_unprocessable_entity(exception)
     render json: { error: exception.message }, status: :unprocessable_entity
